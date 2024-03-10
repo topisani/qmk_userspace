3d2ddf5731dcabce7973f0f25b715c836fa51385
Tobias Pisani <topisani@hamsterpoison.com>
2024-03-10 11:16:08 +0100

    move homerow-mod GUI to inverted T position

--- keyboards/splitkb/kyria/rev1/keymaps/topisani/keymap.c
+++ keyboards/splitkb/kyria/rev1/keymaps/topisani/keymap.c
@@ -70,8 +70,8 @@ reg @ <percent>s<space>=<space>LAYOUT36<ret>j<a-i>is<space><ret>dXs.<backspace>,
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_COLEMAK_DH] = LAYOUT36(
-        KC_Q    , KC_W    , KC_F    , KC_P    , KC_B    , ....... , ....... , KC_J    , KC_L    , KC_U    , KC_Y    , KC_BSPC , 
-        HG(A)   , HA(R)   , HS(S)   , HC(T)   , KC_G    , ....... , ....... , KC_M    , HC(N)   , HS(E)   , HA(I)   , HG(O)   , 
+        KC_Q    , KC_W    , HG(F)   , KC_P    , KC_B    , ....... , ....... , KC_J    , KC_L    , HG(U)   , KC_Y    , KC_BSPC , 
+        KC_A    , HA(R)   , HS(S)   , HC(T)   , KC_G    , ....... , ....... , KC_M    , HC(N)   , HS(E)   , HA(I)   , KC_O    , 
         KC_Z    , KC_X    , KC_C    , KC_D    , KC_V    , ....... , ....... , KC_K    , KC_H    , KC_COMM , KC_DOT  , KC_SLSH , 
         ....... , ....... , ....... , XXXXXXX , QK_REP  , KC_SPC  , L_OSL   , QK_AREP , XXXXXXX , ....... , ....... , .......
     ),
